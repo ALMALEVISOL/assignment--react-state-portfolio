cb905b122580d16a93c8a076537322f26845d90d
Alejandro Del Moral <almalevisol@gmail.com>
2018-09-25 17:45:20 -0500

    Hola

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/styles.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+export { default as App } from "./App";
